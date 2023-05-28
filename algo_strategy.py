3f031577d5ff3e12ec60340531ff0f89758abe05
eric liu <ericl7552@gmail.com>
2023-05-28 15:04:45 -0600

    adding basic base functions

--- algo_strategy.py
+++ algo_strategy.py
@@ -13,41 +13,6 @@ class AlgoStrategy(gamelib.AlgoCore):
         random.seed(seed)
         gamelib.debug_write("Random seed: {}".format(seed))
 
-    def initBase(self, game_state: gamelib.GameState):
-        gamelib.debug_write("running")
-        wall_locations = [
-            [2, 13],
-            [3, 13],
-            [4, 12],
-            [5, 11],
-            [7, 11],
-            [7, 10],
-            [8, 9],
-            [9, 8],
-            [10, 7],
-            [11, 6],
-            [12, 5],
-            [13, 5],
-            [14, 5],
-            [15, 5],
-            [16, 5],
-            [17, 5],
-            [18, 6],
-            [19, 7],
-            [20, 8],
-            [20, 9],
-            [21, 10],
-            [22, 11],
-            [23, 12],
-            [24, 13],
-            [25, 13],
-        ]
-        turret_locations = [[3, 12], [24, 12]]
-        game_state.attempt_spawn(WALL, wall_locations)
-
-        game_state.attempt_spawn(TURRET, turret_locations)
-        return game_state
-
     def on_game_start(self, config):
         """
         Read in config and perform any initial setup here
@@ -77,9 +42,42 @@ class AlgoStrategy(gamelib.AlgoCore):
         game engine.
         """
         game_state = gamelib.GameState(self.config, turn_state)
-        if game_state.turn_number == 1:
+        if game_state.turn_number == 0:
             game_state = self.initBase(game_state)
+        game_state = self.fixBase(game_state)
+        game_state = self.addUpgradeTurrets(game_state)
         game_state.submit_turn()
+
+    def fixBase(self, game_state: gamelib.GameState):
+        ####potential upgrade --> if there you can't rebuild whole base try to fill the things in the middle
+        game_state.attempt_spawn(WALL, wall_locations)
+        game_state.attempt_spawn(TURRET, turret_locations)
+        return game_state
+
+    def addUpgradeTurrets(self, game_state: gamelib.GameState):
+        upgrade_turret_locations = [
+            [3, 12],
+            [4, 11],
+            [7, 9],
+            [8, 8],
+            [22, 10],
+            [23, 11],
+            [24, 12],
+        ]
+        game_state.attempt_upgrade(upgrade_turret_locations)
+        game_state.attempt_spawn(TURRET, upgrade_turret_locations)
+
+    def upgradeWalls(self, game_state: gamelib.GameState):
+        upgrade_wall_locations = [
+            [2, 13],
+            [3, 13],
+            [4, 12],
+            [23, 12],
+            [24, 13],
+            [25, 13],
+        ]
+        game_state.attempt_upgrade(upgrade_wall_locations)
+        return game_state
 
     def on_action_frame(self, action_frame_game_state):
         """
@@ -89,6 +87,40 @@ class AlgoStrategy(gamelib.AlgoCore):
         They can be handled in this function.
         """
         ###maybe try to store what attack they are using....
+
+    def initBase(self, game_state: gamelib.GameState):
+        global wall_locations, turret_locations
+        wall_locations = [
+            [2, 13],
+            [3, 13],
+            [4, 12],
+            [5, 11],
+            [7, 11],
+            [7, 10],
+            [8, 9],
+            [9, 8],
+            [10, 7],
+            [11, 6],
+            [12, 5],
+            [13, 5],
+            [14, 5],
+            [15, 5],
+            [16, 5],
+            [17, 5],
+            [18, 6],
+            [19, 7],
+            [20, 8],
+            [20, 9],
+            [21, 10],
+            [22, 11],
+            [23, 12],
+            [24, 13],
+            [25, 13],
+        ]
+        turret_locations = [[3, 12], [24, 12]]
+        game_state.attempt_spawn(WALL, wall_locations)
+        game_state.attempt_spawn(TURRET, turret_locations)
+        return game_state
 
 
 if __name__ == "__main__":
