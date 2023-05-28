907a5d41f1c1b6f6577982fb0e35ad645b2a79a5
eric liu <ericl7552@gmail.com>
2023-05-28 14:33:56 -0600

    fresh start

--- algo_strategy.py
+++ algo_strategy.py
@@ -3,26 +3,68 @@ import math
 import warnings
 from sys import maxsize
 import json
-from customStrategies import *
+import gamelib
+
 
 class AlgoStrategy(gamelib.AlgoCore):
     def __init__(self):
         super().__init__()
         seed = random.randrange(maxsize)
         random.seed(seed)
-        gamelib.debug_write('Random seed: {}'.format(seed))
+        gamelib.debug_write("Random seed: {}".format(seed))
+
+    def initBase(game_state: gamelib.GameState):
+        gamelib.debug_write("running")
+        wall_locations = [
+            [2, 13],
+            [3, 13],
+            [4, 12],
+            [5, 11],
+            [7, 11],
+            [7, 10],
+            [8, 9],
+            [9, 8],
+            [10, 7],
+            [11, 6],
+            [12, 5],
+            [15, 5],
+            [16, 5],
+            [17, 5],
+            [18, 6],
+            [19, 7],
+            [20, 8],
+            [20, 9],
+            [21, 10],
+            [22, 11],
+            [23, 12],
+            [24, 13],
+            [25, 13],
+        ]
+        turret_locations = [[3, 12], [24, 12]]
+        game_state.attempt_spawn(WALL, wall_locations)
+
+        game_state.attempt_spawn(TURRET, turret_locations)
+        return game_state
 
     def on_game_start(self, config):
-        """ 
-        Read in config and perform any initial setup here 
         """
-        gamelib.debug_write('Configuring your custom algo strategy...')
+        Read in config and perform any initial setup here
+        """
+        gamelib.debug_write("Configuring your custom algo strategy...")
         self.config = config
 
         """
         making the WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR variables
         """
-        initGlobal(config)
+        global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, MP, SP
+        WALL = config["unitInformation"][0]["shorthand"]
+        SUPPORT = config["unitInformation"][1]["shorthand"]
+        TURRET = config["unitInformation"][2]["shorthand"]
+        SCOUT = config["unitInformation"][3]["shorthand"]
+        DEMOLISHER = config["unitInformation"][4]["shorthand"]
+        INTERCEPTOR = config["unitInformation"][5]["shorthand"]
+        MP = 1
+        SP = 0
 
     def on_turn(self, turn_state):
         """
@@ -34,16 +76,15 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         game_state = gamelib.GameState(self.config, turn_state)
         if game_state.turn_number == 1:
-            game_state = initBase(game_state)
+            game_state = self.initBase(game_state)
         game_state.submit_turn()
-
 
     def on_action_frame(self, action_frame_game_state):
         """
         After each deploy phase, the game engine will run the action phase of the round.
-        The action phase is made up of a sequence of distinct frames. 
-        Each of these frames is sent to the algo in order. 
-        They can be handled in this function. 
+        The action phase is made up of a sequence of distinct frames.
+        Each of these frames is sent to the algo in order.
+        They can be handled in this function.
         """
         ###maybe try to store what attack they are using....
 
